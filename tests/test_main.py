838111a7518a593d5af74d0eaaa6b9be778419b4
Hadi Zaki Alqattan <alqattanhadizaki@gmail.com>
2020-09-18 15:48:29 +0300

    Add: pycln/__main__.py tests.

---
+++ tests/test_main.py
@@ -0,0 +1,23 @@
+"""`pycln/__main__.py` tests."""
+from importlib import import_module
+
+from .utils import sysu
+
+
+class TestMain:
+
+    """`__main__.py` side effects tests."""
+
+    def test_main(self):
+        msg = "It looks like pycln/__main__.py has %s!"
+        with sysu.hide_sys_argv():
+            with sysu.std_redirect(sysu.STD.ERR) as stream:
+                try:
+                    try:
+                        import_module("pycln.__main__")
+                    except ModuleNotFoundError:
+                        assert False, msg % "deleted"
+                except SystemExit as err:
+                    assert err.code == 1 and stream.getvalue(), msg % "modified"
+                else:
+                    assert False, msg % "modified"
