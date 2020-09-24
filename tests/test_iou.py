d742bdf7b6dc97480b0fcfa36a4080d90b462dff
Hadi Zaki Alqattan <alqattanhadizaki@gmail.com>
2020-09-24 20:25:17 +0300

    Support win path with set_mode for the shitty OS.

--- tests/test_iou.py
+++ tests/test_iou.py
@@ -54,7 +54,7 @@ class TestIOU:
     def test_safe_read(self, content, expec_code, expec_err, chmod):
         with pytest.raises(expec_err):
             with sysu.reopenable_temp_file(content) as tmp_path:
-                set_mode(tmp_path, chmod)
+                set_mode(str(tmp_path), chmod)
                 # default param: permissions: tuple = (os.R_OK, os.W_OK).
                 source_code, _ = iou.safe_read(tmp_path)
                 assert source_code == expec_code
@@ -82,7 +82,7 @@ class TestIOU:
     def test_safe_write(self, fixed_lines, expec_code, expec_err, chmod):
         with pytest.raises(expec_err):
             with sysu.reopenable_temp_file("".join(fixed_lines)) as tmp_path:
-                set_mode(tmp_path, chmod)
+                set_mode(str(tmp_path), chmod)
                 iou.safe_write(tmp_path, fixed_lines, "utf-8")
                 with open(tmp_path) as tmp:
                     assert tmp.read() == expec_code
