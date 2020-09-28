45e39b96a23e3379e69ef15b759da21c512d20fc
Hadi Zaki Alqattan <alqattanhadizaki@gmail.com>
2020-09-28 16:00:38 +0300

    Add: edit docs buttons.

---
+++ docs/plugins/edit_button.js
@@ -0,0 +1,9 @@
+const editButton = (hook, vm) => {
+  hook.beforeEach(function (html) {
+    let editButton = window.$docsify.editButton;
+    let url = `https://github.com/${editButton.username}/${editButton.repoName}/tree/${editButton.branch}/${editButton.docsPath}/${vm.route.file}`;
+    let editHtml = "[" + editButton.editText + "](" + url + ")";
+    return editHtml + "<br/><br/>\n" + html;
+  });
+};
+window.$docsify.plugins.push(editButton);
