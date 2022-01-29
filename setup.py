9ed66c98ba1b9d64489d7e388755877b48662997
Hadi Alqattan <alqattanhadizaki@gmail.com>
2022-01-29 12:43:22 +0300

    Allow Github to detect the package.

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+# flake8: noqa
+
+#############################################################################################
+#                                                                                           #
+# This file has been copied from:                                                           #
+# https://github.com/Textualize/rich/blob/672f794dd495aa99611c9cfb002bf4b8e440aa46/setup.py #
+#                                                                                           #
+# ----------------------------------------------------------------------------------------- #
+#                                                                                           #
+# This is a shim to hopefully allow Github to detect the package,                           #
+# build is done with poetry.                                                                #
+#                                                                                           #
+#############################################################################################
+
+import setuptools
+
+if __name__ == "__main__":
+    setuptools.setup(name="pycln")
