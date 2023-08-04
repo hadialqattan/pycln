add8a0efc1c7ead2dfb43907a99a93dfb2fa2b66
Hadi Alqattan <alqattanhadizaki@gmail.com>
2023-08-04 15:44:47 +0300

    Add: tests

--- tests/test_iou.py
+++ tests/test_iou.py
@@ -1,6 +1,7 @@
 """pycln/utils/iou.py tests."""
 # pylint: disable=R0201,W0613
 import os
+from pathlib import Path
 from typing import List
 from unittest import mock
 
@@ -10,6 +11,7 @@ from oschmod import set_mode
 from pycln import ISWIN
 from pycln.utils import iou
 from pycln.utils._exceptions import (
+    InitFileDoesNotExistError,
     ReadPermissionError,
     UnparsableFile,
     WritePermissionError,
@@ -76,9 +78,10 @@ class TestIOU:
             raise sysu.Pass()
 
     @pytest.mark.parametrize(
-        "content, expec_code, expec_newline, expec_err, chmod",
+        "file_path, content, expec_code, expec_newline, expec_err, chmod",
         [
             pytest.param(
+                None,  # None means using the generated temp file path.
                 "print('Hello')",
                 "print('Hello')",
                 iou.LF,
@@ -87,6 +90,16 @@ class TestIOU:
                 id="bast case",
             ),
             pytest.param(
+                "DoesNotExistInit/__init__.py",
+                None,
+                None,
+                None,
+                InitFileDoesNotExistError,
+                0o0644,
+                id="Init file does not exist",
+            ),
+            pytest.param(
+                None,
                 "code...",
                 None,
                 None,
@@ -98,6 +111,7 @@ class TestIOU:
                 ),
             ),
             pytest.param(
+                None,
                 "code...",
                 None,
                 None,
@@ -109,6 +123,7 @@ class TestIOU:
                 ),
             ),
             pytest.param(
+                None,
                 #: Make conflict between BOM and encoding Cookie.
                 #: For more information: https://bit.ly/32o3eVl
                 "\ufeff\n# -*- coding: utf-32 -*-\nbad encoding",
@@ -119,6 +134,7 @@ class TestIOU:
                 id="bad encoding",
             ),
             pytest.param(
+                None,
                 "try: pass\x0c;\nfinally: pass",
                 None,
                 None,
@@ -127,6 +143,7 @@ class TestIOU:
                 id="form feed char",
             ),
             pytest.param(
+                None,
                 "print('Hello')\r\n",
                 "print('Hello')\n\n",
                 iou.CRLF,
@@ -135,6 +152,7 @@ class TestIOU:
                 id="detect CRLF",
             ),
             pytest.param(
+                None,
                 "print('Hello')\n",
                 "print('Hello')\n",
                 iou.LF,
@@ -148,9 +166,17 @@ class TestIOU:
         ],
     )
     def test_safe_read(
-        self, content: str, expec_code: str, expec_newline: str, expec_err, chmod: int
+        self,
+        file_path: str,
+        content: str,
+        expec_code: str,
+        expec_newline: str,
+        expec_err,
+        chmod: int,
     ):
         with pytest.raises(expec_err):
+            if file_path:
+                iou.safe_read(Path(file_path))
             if expec_newline:
                 content = content.replace(os.linesep, expec_newline)
             with sysu.reopenable_temp_file(content) as tmp_path:
